0f6ae320905f3fdc8c2de2054522c4b0b51660de
Etienne Juffard <etiennejuffard@gmail.com>
2020-07-30 15:20:39 +0200

    Twig templating system start

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 require_once (__DIR__ .'/vendor/autoload.php');
 
+$title_name = "Site web d'Etienne Juffard";
+
+$title = array(
+    'name' => $title_name,
+);
+
 $loader = new \Twig\Loader\FilesystemLoader('./src/View/Template');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '%kernel.cache_dir%/twig',
-]);
+$twig = new \Twig\Environment($loader);
+$twig->addGlobal("title", $title);
 
 $template = $twig->load('base.html.twig');
 
-$twig->render('home.html.twig');
+echo $twig->render('home.html.twig');
