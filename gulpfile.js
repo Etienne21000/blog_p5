695a245db912aa76340c18031882028ff6845cbe
Etienne Juffard <etiennejuffard@gmail.com>
2020-11-20 09:39:18 +0100

    P_5 Refactoring twig templating and views

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const runSequence = require('gulp4-run-sequence');
+const watch = require('gulp-watch');
+const clean = require('gulp-clean');
+
 
 sass.compiler = require('node-sass');
 
@@ -12,5 +17,27 @@ const files = {
 gulp.task('sass', function() {
     return gulp.src('Public/scss/main.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('Public/css/main.min.css'));
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('Public/main-css'));
+});
+
+gulp.task('clean-css', function () {
+    return gulp.src('Public/main-css', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', async function(callback) {
+    runSequence(
+        ['clean-css'],
+        ['sass'],
+        callback);
+});
+
+gulp.task('watch', async function() {
+    // Endless stream mode
+    gulp.watch([
+            './sass/**/*.scss',
+            'gulpfile.js'
+        ],
+        gulp.series('build'));
 });
